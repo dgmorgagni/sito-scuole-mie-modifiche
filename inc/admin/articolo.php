f4c1a391057a48fc41f961bb13f4b796bb91eefc
dgmorgagni <79083735+dgmorgagni@users.noreply.github.com>
2023-07-15 17:44:37 +0200

    Update articolo.php

--- inc/admin/articolo.php
+++ inc/admin/articolo.php
@@ -52,8 +52,7 @@ function dsi_add_articolo_metaboxes() {
     $cmb_abstrat->add_field( array(
         'id' => $prefix . 'tipologia',
         'name'        => __( 'Tipologia articolo *', 'design_scuole_italia' ),
-        'type'             => 'taxonomy_multicheck_inline',
-	'select_all_button' => false,
+        'type'             => 'taxonomy_radio_inline',
         'show_option_none' => false,
         'taxonomy'       => 'tipologia-articolo',
         'remove_default' => 'true',
